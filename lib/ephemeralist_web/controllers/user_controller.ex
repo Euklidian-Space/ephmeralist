c86afc5b14a0c871ab03bc33f3172a2d7b739c8d
data-kid <gabriel.grover@gmail.com>
2018-07-27 12:53:52 -0700

    abstracted registration token creation

--- lib/ephemeralist_web/controllers/user_controller.ex
+++ lib/ephemeralist_web/controllers/user_controller.ex
@@ -18,14 +18,17 @@ defmodule EphemeralistWeb.UserController do
 
   def create(conn, %{"name" => _, "username" => _} = params) do 
     with {:ok, user} <- Accounts.register_user(params),
-         {:ok, token, _} <- 
-           Guardian.encode_and_sign(
-             user, %{"lifespan" => @token_lifespan}, 
-             token_type: "refresh", 
-             ttl: {4, :weeks}
-           )
+         {:ok, token, _} <- registration_token(user)
     do 
       render(conn, "userToken.json", token: token)
     end 
+  end 
+
+  defp registration_token(user) do 
+    Guardian.encode_and_sign(
+      user, %{"lifespan" => @token_lifespan}, 
+      token_type: "refresh", 
+      ttl: {4, :weeks}
+    )
   end 
 end 
